af22c45ca59b987b610023d4664df80f5079ccd3
oddlyspaced <oddlyspaced1@gmail.com>
2020-05-24 05:48:35 +0530

    DenyService: Log when service connected Shows "Service Connected" when acc. service is turned on

--- app/src/main/java/com/oddlyspaced/deny/DenyService.kt
+++ app/src/main/java/com/oddlyspaced/deny/DenyService.kt
@@ -1,9 +1,16 @@
 package com.oddlyspaced.deny
 
 import android.accessibilityservice.AccessibilityService
+import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 
 class DenyService: AccessibilityService() {
+
+    private val tag = "DenyService"
+
+    override fun onServiceConnected() {
+        Log.d(tag, "Service Connected")
+    }
 
     override fun onInterrupt() {
         TODO("Not yet implemented")

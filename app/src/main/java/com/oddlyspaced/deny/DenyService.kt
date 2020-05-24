07e1a375daf9f5bff46abdab8643802897c5f776
oddlyspaced <oddlyspaced1@gmail.com>
2020-05-24 05:41:33 +0530

    DenyService: Setup skeleton service

---
+++ app/src/main/java/com/oddlyspaced/deny/DenyService.kt
@@ -0,0 +1,16 @@
+package com.oddlyspaced.deny
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class DenyService: AccessibilityService() {
+
+    override fun onInterrupt() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        TODO("Not yet implemented")
+    }
+
+}

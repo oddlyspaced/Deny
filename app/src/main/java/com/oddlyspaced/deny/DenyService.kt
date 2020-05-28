15bcfd55a2f5995a3fef05542b0a43271c8c0106
oddlyspaced <oddlyspaced1@gmail.com>
2020-05-28 23:39:43 +0530

    DenyService: Detect permission granting

--- app/src/main/java/com/oddlyspaced/deny/DenyService.kt
+++ app/src/main/java/com/oddlyspaced/deny/DenyService.kt
@@ -1,97 +1,105 @@
 package com.oddlyspaced.deny
 
 import android.accessibilityservice.AccessibilityService
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
+import android.os.Handler
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.graphics.scaleMatrix
 import java.lang.Exception
+import kotlin.random.Random
 
 
 class DenyService : AccessibilityService() {
 
-    private val tag = "DenyService"
+    private val notificationChannelId = "10001"
 
+    private val tag = "DenyService"
+    private lateinit var pkgManager: PackageListManager
+    private val whitelistedPackages = arrayListOf("com.android.systemui", "com.google.android.packageinstaller", "com.android.settings", "com.android.packageinstaller", "com.google.android.permissioncontroller", "com.android.permissioncontroller")
     override fun onServiceConnected() {
         Log.d(tag, "Service Connected")
-
-        var intent = Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-        intent.data = (Uri.parse("package:com.whatsapp"))
-        intent.flags = FLAG_ACTIVITY_NEW_TASK
-        startActivity(intent)
-
-        Log.e("why", "no u come")
+        pkgManager = PackageListManager(applicationContext)
+        createNotificationChannel()
     }
 
     override fun onInterrupt() {
         TODO("Not yet implemented")
     }
 
-    var currentScreen = -1
-    var perms = arrayOf(
-        "Call logs",
-        "Camera",
-        "Contacts",
-        "Location",
-        "Microphone",
-        "SMS",
-        "Telephone",
-        "Storage"
-    )
-    private var permcounter = 0
+    private var packageInContext = ""
+    private var grantedPermissions = ArrayList<String>()
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        if (event != null && event.source != null) {
-            if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-                try {
-                    if (currentScreen == -1) {
-                        event.source.refresh()
-                        val res = event.source.findAccessibilityNodeInfosByText("App permissions")[0].parent
-                        res.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        currentScreen = 1
-                    }
-                    else if (currentScreen == 1) {
-                        for (p in perms) {
-                            val perm = event.source.findAccessibilityNodeInfosByText(p)[0].parent
-                            perm.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        }
-                        performGlobalAction(GLOBAL_ACTION_BACK)
-                    }
+        if (event?.packageName == null)
+            return
+        if (event.packageName.toString() != packageInContext && !whitelistedPackages.contains(event.packageName.toString())) {
+            packageInContext = event.packageName.toString()
+            grantedPermissions = pkgManager.getGrantedPermissions(packageInContext)
+            checkPerms()
+        }
+    }
 
-                    /*if (currentScreen == -1) {
-                        // app info screen
-                        val res =
-                            event.source.findAccessibilityNodeInfosByText("Permissions")[0].parent
-                        res.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        currentScreen = 0
-                    } else if (currentScreen == 0) {
-                        // app permission screen
-                        event.source.refresh()
-                        if (permcounter == perms.size) {
-                            currentScreen = 5
-                            return
-                        }
-                        val perm =
-                            event.source.findAccessibilityNodeInfosByText(perms[permcounter])[0].parent
-                        permcounter++
-                        perm.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        currentScreen = 1
-                    } else if (currentScreen == 1) {
-                        event.source.refresh()
-                        // allow deny page
-                        val deny = event.source.findAccessibilityNodeInfosByText("Deny")[0]
-                        deny.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        performGlobalAction(GLOBAL_ACTION_BACK)
-                        currentScreen = 0
-                    }*/
-                } catch (e: Exception) {
-                    Log.e("error", e.toString())
-                    print(e.stackTrace)
+    private fun checkPerms() {
+        Handler().postDelayed({
+            Log.e("conteeeeee", packageInContext)
+            try {
+                val pp = pkgManager.getGrantedPermissions(packageInContext)
+                if (pp != grantedPermissions) {
+                    val diff = pp.subtract(grantedPermissions).toList()[0]
+                    Log.e("DIFFERENT", diff)
+                    createNotification(pkgManager.checkPermGroup(diff))
+                    grantedPermissions = pp
+                    Log.e("pp", pp.toString())
                 }
             }
+            catch (e: Exception) {
+                Log.e("Error", e.toString())
+            }
+            checkPerms()
+        }, 100)
+    }
+
+    private fun createNotificationChannel() {
+        val name = getString(R.string.app_name)
+        val descriptionText = getString(R.string.app_name)
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(notificationChannelId, name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createNotification(permission: Int) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, notificationChannelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(pkgManager.getPermGroupName(permission))
+            .setContentText("TEXT")
+            .setPriority(NotificationCompat.PRIORITY_MIN)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setWhen(System.currentTimeMillis())
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(permission, builder.build())
         }
     }
 

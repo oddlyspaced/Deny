b8180f81786621f7529aaa7b9e1395c5d3184ba5
oddlyspaced <oddlyspaced1@gmail.com>
2020-05-30 06:40:52 +0530

    MainActivity: Detect if service is on

--- app/src/main/java/com/oddlyspaced/deny/activity/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/deny/activity/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.oddlyspaced.deny.activity
 
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Animatable
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.provider.Settings
 import android.util.Log
+import android.view.accessibility.AccessibilityManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.oddlyspaced.deny.util.PackageListManager
@@ -32,8 +39,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (isServiceEnabled()) {
+            startActivity(Intent(this, TabbedActivity::class.java))
+        }
         animateIcon()
         setupOnTouch()
+        isServiceEnabled()
         Log.e("time", Calendar.getInstance().get(Calendar.HOUR).toString())
     }
 
@@ -52,8 +63,86 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupOnTouch() {
         viewAccessibiltyTouch.setOnClickListener {
+            Toast.makeText(applicationContext, "Find \"Deny\" and enable it", Toast.LENGTH_SHORT).show()
+            Handler().postDelayed({
+                val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            }, 500)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isServiceEnabled()) {
             startActivity(Intent(this, TabbedActivity::class.java))
         }
     }
+
+    private fun isServiceEnabled(): Boolean {
+        val am = applicationContext.getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+        val enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
+        for (service in enabledServices) {
+            if (service.resolveInfo.serviceInfo.name.contains(packageName))
+                return true
+        }
+        return false
+    }
+
+    /*
+    public static boolean isAccessibilityServiceEnabled(Context context, Class<? extends AccessibilityService> service) {
+    AccessibilityManager am = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+    List<AccessibilityServiceInfo> enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
+
+    for (AccessibilityServiceInfo enabledService : enabledServices) {
+        ServiceInfo enabledServiceInfo = enabledService.getResolveInfo().serviceInfo;
+        if (enabledServiceInfo.packageName.equals(context.getPackageName()) && enabledServiceInfo.name.equals(service.getName()))
+            return true;
+    }
+
+    return false;
+}
+     */
+
+    /*
+    public boolean isAccessibilityEnabled() {
+    int accessibilityEnabled = 0;
+    final String LIGHTFLOW_ACCESSIBILITY_SERVICE = "com.example.test/com.example.text.ccessibilityService";
+    boolean accessibilityFound = false;
+    try {
+        accessibilityEnabled = Settings.Secure.getInt(this.getContentResolver(),android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+        Log.d(LOGTAG, "ACCESSIBILITY: " + accessibilityEnabled);
+    } catch (SettingNotFoundException e) {
+        Log.d(LOGTAG, "Error finding setting, default accessibility to not found: " + e.getMessage());
+    }
+
+    TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
+
+    if (accessibilityEnabled==1) {
+        Log.d(LOGTAG, "***ACCESSIBILIY IS ENABLED***: ");
+
+        String settingValue = Settings.Secure.getString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+        Log.d(LOGTAG, "Setting: " + settingValue);
+        if (settingValue != null) {
+             TextUtils.SimpleStringSplitter splitter = mStringColonSplitter;
+             splitter.setString(settingValue);
+             while (splitter.hasNext()) {
+                 String accessabilityService = splitter.next();
+                 Log.d(LOGTAG, "Setting: " + accessabilityService);
+                 if (accessabilityService.equalsIgnoreCase(ACCESSIBILITY_SERVICE_NAME)){
+                     Log.d(LOGTAG, "We've found the correct setting - accessibility is switched on!");
+                     return true;
+                 }
+             }
+        }
+
+        Log.d(LOGTAG, "***END***");
+    }
+    else {
+        Log.d(LOGTAG, "***ACCESSIBILIY IS DISABLED***");
+    }
+    return accessibilityFound;
+}
+     */
 
 }
